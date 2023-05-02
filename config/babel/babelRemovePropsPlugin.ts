06d6a52204bff74350857c9635a7749704febc88
Evgenii <evgenypetrov5230@gmail.com>
2023-05-02 14:52:23 +0300

    ci pipeline

--- config/babel/babelRemovePropsPlugin.ts
+++ config/babel/babelRemovePropsPlugin.ts
@@ -1,21 +1,23 @@
-import { PluginItem } from '@babel/core';
+// import { PluginItem } from '@babel/core';
+// TODO
+// export default function (): PluginItem {
+//     return {
+//         visitor: {
+//             Program(path, state) {
+//                 const forbidden = state.opts.props || [];
+//
+//                 path.traverse({
+//                     JSXIdentifier(current) {
+//                         const nodeName = current.node.name;
+//
+//                         if (forbidden.includes(nodeName)) {
+//                             current.parentPath.remove();
+//                         }
+//                     },
+//                 });
+//             },
+//         },
+//     };
+// }
 
-export default function (): PluginItem {
-    return {
-        visitor: {
-            Program(path, state) {
-                const forbidden = state.opts.props || [];
-
-                path.traverse({
-                    JSXIdentifier(current) {
-                        const nodeName = current.node.name;
-
-                        if (forbidden.includes(nodeName)) {
-                            current.parentPath.remove();
-                        }
-                    },
-                });
-            },
-        },
-    };
-}
+export {};
