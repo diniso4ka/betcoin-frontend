88d2dd0aa5fc3799e2f43ffbc998b62aed8744fc
Evgenii <evgenypetrov5230@gmail.com>
2023-05-10 17:04:03 +0300

    router

---
+++ src/app/providers/router/config/routeConfig.tsx
@@ -0,0 +1,10 @@
+import { MainPage } from '@/pages/MainPage';
+import { getRouteMain } from '@/shared/const/router';
+import { AppRoutesProps } from '@/shared/types/router';
+
+export const routerConfig: AppRoutesProps[] = [
+    {
+        path: getRouteMain(),
+        element: <MainPage />,
+    },
+];
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,6 @@
 const MainPage = () => {
     // eslint-disable-next-line i18next/no-literal-string
-    return <div data-testid='MainPage'>MainPage</div>;
+    return <div>MainPage</div>;
 };
 
 export default MainPage;
