05a5ee3d25d2c3e49e87c4099a8a68aca9924536
Evgenii <evgenypetrov5230@gmail.com>
2023-05-10 18:37:47 +0300

    AppLink

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,36 @@
+import cn from 'classnames';
+import { memo, ReactNode } from 'react';
+
+import { Link, LinkProps } from 'react-router-dom';
+
+import { AppLinkSize, AppLinkTheme } from './types';
+
+import s from './AppLink.module.scss';
+
+interface AppLinkProps extends LinkProps {
+    children: ReactNode;
+    className?: string;
+    theme?: AppLinkTheme;
+    size?: AppLinkSize;
+}
+
+const AppLinkComponent = (props: AppLinkProps) => {
+    const {
+        className,
+        children,
+        to,
+        theme = 'THEME_WHITE',
+        size = 'SIZE_M',
+        ...rest
+    } = props;
+
+    const classnames = cn(s.link, className, s[theme], s[size]);
+
+    return (
+        <Link to={to} className={classnames} {...rest}>
+            {children}
+        </Link>
+    );
+};
+
+export const AppLink = memo(AppLinkComponent);
---
+++ src/shared/ui/AppLink/types.ts
@@ -0,0 +1,3 @@
+export type AppLinkTheme = 'THEME_WHITE';
+
+export type AppLinkSize = 'SIZE_M';
